0523a464b15ba2083ba7a032c22b486d2365ef78
frida <frida.ruiz2532@gmail.com>
2019-08-18 16:32:30 -0300

    testing x2

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const { getUsers, getUser, createUser, editUser, deleteUser } = require('../controllers/user');
 
 router.get('/all', getUsers);
-router.get('/find', getUser);
+router.post('/find', getUser);
 router.post('/signup', createUser);
 router.patch('/edit', editUser);
 router.delete('/delete', deleteUser);
